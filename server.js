79395e45e8e0d6abcd3b3cab34d767e28448f329
kodanda12 <68196991+kodanda12@users.noreply.github.com>
2025-06-27 00:54:02 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "trivastara-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "socket.io": "^4.7.1"
+  }
+}
